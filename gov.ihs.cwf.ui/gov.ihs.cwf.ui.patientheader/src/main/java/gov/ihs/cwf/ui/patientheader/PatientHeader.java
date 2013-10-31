fb01a9154e7607ee7e27bd77d82bd2b7b3cc02e8
Douglas Martin <dkmartin@regenstrief.org>
2013-10-31 15:37:17 -0400

    Use DateUtil method for formatting age.

--- gov.ihs.cwf.ui/gov.ihs.cwf.ui.patientheader/src/main/java/gov/ihs/cwf/ui/patientheader/PatientHeader.java
+++ gov.ihs.cwf.ui/gov.ihs.cwf.ui.patientheader/src/main/java/gov/ihs/cwf/ui/patientheader/PatientHeader.java
@@ -9,15 +9,16 @@
  */
 package gov.ihs.cwf.ui.patientheader;
 
+import gov.ihs.cwf.context.PatientContext;
+import gov.ihs.cwf.domain.Patient;
+import gov.ihs.cwf.ui.patientselection.PatientSelection;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.carewebframework.api.property.PropertyUtil;
 import org.carewebframework.common.DateUtil;
 import org.carewebframework.ui.FrameworkController;
-import gov.ihs.cwf.context.PatientContext;
-import gov.ihs.cwf.domain.Patient;
-import gov.ihs.cwf.ui.patientselection.PatientSelection;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Events;
@@ -30,88 +31,90 @@ import org.zkoss.zul.Label;
  * @author dmartin
  */
 public class PatientHeader extends FrameworkController implements PatientContext.IPatientContextEvent {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private static final Log log = LogFactory.getLog(PatientHeader.class);
-	
-	private Label patientHeader;
-	
-	private String noSelectionMessage;
-	
-	private Component root;
-	
-	public void onClick$select() {
-		PatientSelection.show();
-	}
-	
-	public void onForceSelection() {
-		PatientSelection.show();
-	}
-	
-	/**
-	 * Returns true if a patient selection should be forced when a null patient context exists.
-	 * 
-	 * @return
-	 */
-	private boolean forcePatientSelection() {
-	
-		try {
-			return "Y".equals(PropertyUtil.getValue("CAREWEB.PATIENT.FORCE.SELECT", null));
-		}
-		catch (Exception e) {
-			return false;
-		}
-	}
-	
-	@Override
-	public void doAfterCompose(Component comp) throws Exception {
-		super.doAfterCompose(comp);
-		root = comp;
-		noSelectionMessage = patientHeader.getValue();
-		committed();
-		
-		// call the patient selection routine at login, if the user preference is set
-		if (PatientContext.getCurrentPatient() == null && forcePatientSelection())
-			Events.echoEvent("onForceSelection", comp, null);
-	}
-	
-	@Override
-	public void canceled() {
-	}
-	
-	@Override
-	public void committed() {
-		Patient patient = PatientContext.getCurrentPatient();
-		
-		if (log.isDebugEnabled()) 
-			log.debug("patient: " + patient);
-
-		String text = "";
-		
-		if (patient == null) {
-			text = noSelectionMessage;
-		} else {
-		    StringBuilder sb = new StringBuilder(patient.getFullName());
-			String mrn = patient.getMedicalRecordNumber(); // May be null!
-			sb.append("  #").append(mrn == null ? "Unknown" : mrn);
-			sb.append("@").append(patient.getInstitution().getAbbreviation());
-			sb.append("   (").append(patient.getGender()).append(")");
-			sb.append("  Age: ").append(patient.getAgeForDisplay());
-			
-			if (patient.getDeathDate() != null)
-				sb.append("  Died: ").append(DateUtil.formatDate(patient.getDeathDate()));
-			
-			text = sb.toString();
-		}
-		
-		patientHeader.setValue(text);
-		Clients.resize(root);
-	}
-	
-	@Override
-	public String pending(boolean silent) {
-		return null;
-	}
-	
+    
+    private static final long serialVersionUID = 1L;
+    
+    private static final Log log = LogFactory.getLog(PatientHeader.class);
+    
+    private Label patientHeader;
+    
+    private String noSelectionMessage;
+    
+    private Component root;
+    
+    public void onClick$select() {
+        PatientSelection.show();
+    }
+    
+    public void onForceSelection() {
+        PatientSelection.show();
+    }
+    
+    /**
+     * Returns true if a patient selection should be forced when a null patient context exists.
+     * 
+     * @return
+     */
+    private boolean forcePatientSelection() {
+        
+        try {
+            return "Y".equals(PropertyUtil.getValue("CAREWEB.PATIENT.FORCE.SELECT", null));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        root = comp;
+        noSelectionMessage = patientHeader.getValue();
+        committed();
+        
+        // call the patient selection routine at login, if the user preference is set
+        if (PatientContext.getCurrentPatient() == null && forcePatientSelection()) {
+            Events.echoEvent("onForceSelection", comp, null);
+        }
+    }
+    
+    @Override
+    public void canceled() {
+    }
+    
+    @Override
+    public void committed() {
+        Patient patient = PatientContext.getCurrentPatient();
+        
+        if (log.isDebugEnabled()) {
+            log.debug("patient: " + patient);
+        }
+        
+        String text = "";
+        
+        if (patient == null) {
+            text = noSelectionMessage;
+        } else {
+            StringBuilder sb = new StringBuilder(patient.getFullName());
+            String mrn = patient.getMedicalRecordNumber(); // May be null!
+            sb.append("  #").append(mrn == null ? "Unknown" : mrn);
+            sb.append("@").append(patient.getInstitution().getAbbreviation());
+            sb.append("   (").append(patient.getGender()).append(")");
+            sb.append("  Age: ").append(DateUtil.formatAge(patient.getBirthDate()));
+            
+            if (patient.getDeathDate() != null) {
+                sb.append("  Died: ").append(DateUtil.formatDate(patient.getDeathDate()));
+            }
+            
+            text = sb.toString();
+        }
+        
+        patientHeader.setValue(text);
+        Clients.resize(root);
+    }
+    
+    @Override
+    public String pending(boolean silent) {
+        return null;
+    }
+    
 }
