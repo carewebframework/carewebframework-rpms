dbbb1ae847c8d241a442776ee741239543eb23b6
Douglas Martin <dkmartin@regenstrief.org>
2015-09-01 17:36:38 -0400

    Merge in Phil Salmon's changes (additional properties).

--- org.carewebframework.rpms.api-parent/org.carewebframework.rpms.api.core/src/main/java/org/carewebframework/rpms/api/domain/Immunization.java
+++ org.carewebframework.rpms.api-parent/org.carewebframework.rpms.api.core/src/main/java/org/carewebframework/rpms/api/domain/Immunization.java
@@ -9,10 +9,6 @@
  */
 package org.carewebframework.rpms.api.domain;
 
-import ca.uhn.fhir.model.dstu2.resource.Encounter;
-import ca.uhn.fhir.model.dstu2.resource.Location;
-import ca.uhn.fhir.model.dstu2.resource.Practitioner;
-
 import org.apache.commons.lang.math.NumberUtils;
 
 import org.carewebframework.api.domain.DomainFactoryRegistry;
@@ -20,6 +16,10 @@ import org.carewebframework.cal.api.encounter.EncounterRelated;
 import org.carewebframework.common.JSONUtil;
 import org.carewebframework.common.StrUtil;
 import org.carewebframework.vista.mbroker.FMDate;
+
+import ca.uhn.fhir.model.dstu2.resource.Encounter;
+import ca.uhn.fhir.model.dstu2.resource.Location;
+import ca.uhn.fhir.model.dstu2.resource.Practitioner;
 
 public class Immunization extends EncounterRelated {
     
@@ -47,6 +47,18 @@ public class Immunization extends EncounterRelated {
     
     private String cvx;
     
+    private FMDate visDate;
+    
+    private String adminNotes;
+    
+    private String manufacturer;
+    
+    private String vfcElig;
+    
+    private String vpedIEN;
+    
+    private String vacOverride;
+    
     public Immunization() {
         super();
     }
@@ -58,13 +70,14 @@ public class Immunization extends EncounterRelated {
      *            [6] ^ Imm IEN [7] ^ Lot [8] ^ Reaction [9] ^ VIS Date [10] ^ Age [11] ^ Visit Date
      *            [12] ^ Provider IEN~Name [13] ^ Inj Site [14] ^ Volume [15] ^ Visit IEN [16] ^
      *            Visit Category [17] ^ Full Name [18] ^ Location IEN~Name [19] ^ Visit Locked [20]
-     *            ^ Event Date/Time [21] ^ Dose Override [22] ^ VPED IEN [23] e.g.,
+     *            ^ Event Date/Time [21] ^ Dose Override [22] ^ VPED IEN [23] ^ VFC Eligibility [
+     *            24] ^ Admin Notes [25] ^ Manufacturer [26]
      *            I^Td-ADULT^09/25/1996^86751^^TD_B^102^^^^61 yrs^25-Sep-1996^^^^553825^A^TD
      *            (ADULT)^3987~EHR TEST SITE^1 I^HEP A^03/17/1995^250950^Home^HEPA^131^^^^59
      *            yrs^17-Mar-1995^^^^2020333^E^HEP A, NOS^4744~OTHER^1
      */
     public Immunization(String value) {
-        String[] pcs = StrUtil.split(value, StrUtil.U, 23);
+        String[] pcs = StrUtil.split(value, StrUtil.U, 26);
         setId(pcs[3]);
         immunization = new CodingProxy(pcs[6], "IMMUNIZATION", pcs[1], pcs[1]);
         
@@ -85,7 +98,14 @@ public class Immunization extends EncounterRelated {
         provider = PCCUtil.parsePractitioner(pcs[12]);
         long visitId = NumberUtils.toLong(pcs[15]);
         setEncounter(visitId > 0 ? DomainFactoryRegistry.fetchObject(Encounter.class, pcs[15]) : null);
-        eventDate = PCCUtil.parseDate(pcs[20].isEmpty() ? pcs[2] : pcs[20]);
+        //eventDate = PCCUtil.parseDate(pcs[20].isEmpty() ? pcs[2] : pcs[20]);
+        eventDate = PCCUtil.parseDate(pcs[2]);
+        visDate = PCCUtil.parseDate(pcs[9]);
+        vacOverride = pcs[21];
+        vpedIEN = pcs[22];
+        vfcElig = pcs[23];
+        adminNotes = pcs[24];
+        manufacturer = pcs[25];
     }
     
     public FMDate getEventDate() {
@@ -172,4 +192,51 @@ public class Immunization extends EncounterRelated {
         this.cvx = cvx;
     }
     
+    public String getVPEDIEN() {
+        return vpedIEN;
+    }
+    
+    public void setVPEDIEN(String vpedIEN) {
+        this.vpedIEN = vpedIEN;
+    }
+    
+    public String getManufacturer() {
+        return manufacturer;
+    }
+    
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+    
+    public String getAdminNotes() {
+        return adminNotes;
+    }
+    
+    public void setAdminNotes(String adminNotes) {
+        this.adminNotes = adminNotes;
+    }
+    
+    public FMDate getVISDate() {
+        return visDate;
+    }
+    
+    public void setVISDate(FMDate visDate) {
+        this.visDate = visDate;
+    }
+    
+    public String getVFCElig() {
+        return vfcElig;
+    }
+    
+    public void setVFCElig(String vfcElig) {
+        this.vfcElig = vfcElig;
+    }
+    
+    public String getVacOverride() {
+        return vacOverride;
+    }
+    
+    public void setVacOverride(String vacOverride) {
+        this.vacOverride = vacOverride;
+    }
 }
